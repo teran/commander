a14d2d7b03e45d3970c3d400a8ca5e90a40b2c12
Igor Shishkin <ishishkin@mirantis.com>
2015-05-05 04:09:27 +0300

    Version bump: 1.0.1
    Signed-off-by: Igor Shishkin <ishishkin@mirantis.com>

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name='commander',
-    version='1.0.0',
+    version='1.0.1',
     description='Parallel command exectution framework',
     author='Igor Shishkin',
     author_email='ishishkin@mirantis.com',
